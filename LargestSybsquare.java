4d323be5eca5892205d6bb4436b3782f5bbf9f9b
roshanzha <roshanjhzha825@gmail.com>
2024-03-14 09:46:41 +0545

    Hello All

---
+++ LargestSybsquare.java
@@ -0,0 +1,57 @@
+// User function Template for Java
+class Solution largestSubsquare{
+    int largestSubsquare(int n, char a[][]) {
+        // code here
+          int[][] top = new int[n][n];
+        int[][] left = new int[n][n];
+
+        // Top metric
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 'X') {
+                    if (i != 0)
+                        top[i][j] = top[i - 1][j] + 1;
+                    else
+                }
+            }
+        }
+
+        // Left metric
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 'X') {
+                    if (j != 0)
+                        left[i][j] = left[i][j - 1] + 1;
+                    else
+                        left[i][j] = 1;
+                }
+            }
+        }
+
+        int maxSubSq = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (top[i][j] == 0 || left[i][j] == 0)
+                    continue;
+
+                int currentValue = Math.min(top[i][j], left[i][j]);
+
+                while (currentValue > 0) {
+                    int top1 = i - currentValue + 1;
+                    int left1 = j - currentValue + 1;
+
+                    if ((left[top1][j] >= currentValue) && (top[i][left1] >= currentValue)) {
+                        maxSubSq = Math.max(maxSubSq, currentValue);
+                        break;
+                    }
+
+                    currentValue--;
+                }
+            }
+        }
+
+        return maxSubSq;
+        
+    }
+};
