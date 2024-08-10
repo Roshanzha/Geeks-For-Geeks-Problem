7c3ea2d70a32e01e87814c1173292f70ba098ce3
roshanzha <roshanjhzha825@gmail.com>
2024-08-10 23:47:23 +0530

    Aug 10

---
+++ Aug10/aug10.java
@@ -0,0 +1,34 @@
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Aug10 {
+    // Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        // add code here
+         int count=0;
+       Node temp=null,start=head,ans=head;
+       while(head!=null && head.next!=null){
+           count++;
+           if(count==k) {
+               temp=head;
+           }
+           head=head.next;
+           
+       }
+       if(temp==null) return start;
+       ans=temp.next;
+       temp.next=null;
+       head.next=start;
+       return ans;
+    }
+}
