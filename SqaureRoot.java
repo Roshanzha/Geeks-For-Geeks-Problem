93edbc5b5241e095e693f43c722934bf54e46ce6
Roshan Jha <87078100+Roshanzha@users.noreply.github.com>
2024-08-13 23:42:00 +0530

    Create SqaureRoot.java

---
+++ SqaureRoot.java
@@ -0,0 +1,11 @@
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution {
+    long floorSqrt(long n) {
+        // Your code here
+        return (int)Math.sqrt(n);
+    }
+}
